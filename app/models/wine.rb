013e95ef0cae75b926fd8e5cf97715bbbcad8dc1
Juan Pablo <juanpablogast@gmail.com>
2021-06-04 20:19:18 -0400

    dependent destroy a modelos

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,6 +1,6 @@
 class Wine < ApplicationRecord
     has_many :wine_strains
-    has_many :strains, through: :wine_strains
+    has_many :strains, through: :wine_strains, dependent: :destroy
 
 
     def addStrainPercent(percents)
